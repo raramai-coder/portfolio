b800a156451b804f4524c9a8d4288673d814bbae
raramai-coder <1846409@students.wits.ac.za>
2022-06-10 12:02:04 +0200

    added javascript navigation

---
+++ navigation.js
@@ -0,0 +1,27 @@
+function navigateTo(pageName) {
+  let navLink = document.getElementById(pageName);
+
+  switch (pageName) {
+    case "home":
+      navLink.href = "/index.html";
+      break;
+    case "about":
+      navLink.href = "/about.html";
+      break;
+    case "design":
+      navLink.href = "/designer.html";
+      break;
+    case "blog":
+      navLink.href = "/blogs/blogs.html";
+      break;
+    case "photography":
+      navLink.href = "/photography/photography.html";
+      break;
+    case "develop":
+      navLink.href = "/developer.html";
+      break;
+    default:
+      console.log("Unable to locate page to navigate to");
+      break;
+  }
+}
