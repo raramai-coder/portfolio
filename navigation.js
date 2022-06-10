34362f07fb5ec5db049977e192519ebef5ce2424
raramai-coder <1846409@students.wits.ac.za>
2022-06-10 19:22:34 +0200

    navigation error attempt to fix

--- navigation.js
+++ navigation.js
@@ -3,22 +3,22 @@ function navigateTo(pageName) {
 
   switch (pageName) {
     case "home":
-      navLink.href = "/index.html";
+      navLink.href = "../index.html";
       break;
     case "about":
-      navLink.href = "/about.html";
+      navLink.href = "../about.html";
       break;
     case "design":
-      navLink.href = "/designer.html";
+      navLink.href = "../designer.html";
       break;
     case "blog":
-      navLink.href = "/blogs/blogs.html";
+      navLink.href = "../blogs/blogs.html";
       break;
     case "photography":
-      navLink.href = "/photography/photography.html";
+      navLink.href = "../photography/photography.html";
       break;
     case "develop":
-      navLink.href = "/developer.html";
+      navLink.href = "../developer.html";
       break;
     default:
       console.log("Unable to locate page to navigate to");
