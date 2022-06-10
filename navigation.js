60bfbfbee340b05ba9de64127a1bb54c182508b9
raramai-coder <1846409@students.wits.ac.za>
2022-06-10 22:57:46 +0200

    attempts to fix navigation error with github pages

--- navigation.js
+++ navigation.js
@@ -27,3 +27,33 @@ function navigateTo(pageName) {
       break;
   }
 }
+
+function navigateToFolder(pageName) {
+  let navLink = document.getElementById(pageName);
+  //let siteURL = "portfolio";
+  let siteURL = "../";
+
+  switch (pageName) {
+    case "home":
+      navLink.href = siteURL + "index.html";
+      break;
+    case "about":
+      navLink.href = siteURL + "about.html";
+      break;
+    case "design":
+      navLink.href = siteURL + "designer.html";
+      break;
+    case "blog":
+      navLink.href = siteURL + "blogs/blogs.html";
+      break;
+    case "photography":
+      navLink.href = siteURL + "photography/photography.html";
+      break;
+    case "develop":
+      navLink.href = siteURL + "developer.html";
+      break;
+    default:
+      console.log("Unable to locate page to navigate to");
+      break;
+  }
+}
