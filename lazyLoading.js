578abe5c7ed56ea4cbab7f0a4c56ed44798aa34c
raramai-coder <71047546+raramai-coder@users.noreply.github.com>
2022-07-04 11:31:02 +0200

    lazy loading test 3

--- lazyLoading.js
+++ lazyLoading.js
@@ -1,5 +1,5 @@
-// const observer = window.lozad();
-// observer.observe();
+const observer = window.lozad();
+//observer.observe();
 
 // const el = document.querySelectorAll('img');
 // const observer = window.lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
@@ -21,14 +21,17 @@
 // observer.triggerLoad(secondImage);
 // observer.observe();
 
-const observer = window.lozad('.lozad',{
-    load:function(el){
-        el.src = el.dataset.src;
-        // el.onload=function(){
-        //     el.classList.add('fade')
-        // }
-    }
-}).observe()
+// const observer = window.lozad('.lozad',{
+//     load:function(el){
+//         el.src = el.dataset.src;
+//         // el.onload=function(){
+//         //     el.classList.add('fade')
+//         // }
+//     }
+// }).observe()
 
 const firstImage = document.querySelector('.firstImage');
 observer.triggerLoad(firstImage);
+
+const secondImage = document.querySelector('.secondImage');
+observer.triggerLoad(secondImage);
