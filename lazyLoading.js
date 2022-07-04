42e04752d3c905c4801782c692460a7848b2ecb0
raramai-coder <71047546+raramai-coder@users.noreply.github.com>
2022-07-04 11:35:51 +0200

    ali lazy loading

--- lazyLoading.js
+++ lazyLoading.js
@@ -1,25 +1,5 @@
 const observer = window.lozad();
-//observer.observe();
-
-// const el = document.querySelectorAll('img');
-// const observer = window.lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
-// observer.observe();
-
-// const observer = window.lozad('.lozad',{
-//     load:function(el){
-//         el.src = el.getAttribute("data-src");
-//     }
-// })
-// observer.observe();
-
-// const firstImage = document.querySelector('.firstImage');
-// // const firstImage = document.getElementById("firstImage");
-// observer.triggerLoad(firstImage);
-
-// // const secondImage = document.querySelector('#secondImage');
-// const secondImage = document.getElementById('secondImage');
-// observer.triggerLoad(secondImage);
-// observer.observe();
+observer.observe();
 
 // const observer = window.lozad('.lozad',{
 //     load:function(el){
