cf621c747024f808101349b7ba450839c37f9c44
raramai-coder <71047546+raramai-coder@users.noreply.github.com>
2022-07-10 08:11:13 +0200

    lozad images

--- photography/photography.js
+++ photography/photography.js
@@ -1,32 +1,30 @@
 function highlightCorrespondingElement(correspondingElement, elementID, enter) {
-    
-    let element = document.getElementById(elementID);
+  //console.log("here");
+  let element = document.getElementById(elementID);
 
-    if(window.innerWidth>600){
-        if(correspondingElement=="image"){
-        highlightCorrespondingImage(element, enter);
-        }else{
-        highlightCorrespondingLink(element, enter);
-        }
-    }else{
-        console.log("here");
+  if (window.innerWidth > 600) {
+    if (correspondingElement == "image") {
+      highlightCorrespondingImage(element, enter);
+    } else {
+      highlightCorrespondingLink(element, enter);
     }
-    
+  } else {
+    console.log("here");
+  }
 }
 
 function highlightCorrespondingImage(image, enter) {
-    if(enter){
-        image.style.opacity = 1; 
-    }else{
-        image.style.opacity = 0;
-    }
-    
+  if (enter) {
+    image.style.opacity = 1;
+  } else {
+    image.style.opacity = 0;
+  }
 }
 
 function highlightCorrespondingLink(link, enter) {
-    if(enter){
-        link.style.color = "#F4978E"; 
-    }else{
-        link.style.color = "#001219"; 
-    }
+  if (enter) {
+    link.style.color = "#F4978E";
+  } else {
+    link.style.color = "#001219";
+  }
 }
